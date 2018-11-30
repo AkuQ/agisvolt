c1ff34dc6eecd82a2d4869459f19241a04157a28
Akseli Kuistio <akuistio@gmail.com>
2018-11-30 23:33:13 +0200

    basic draft for device/measurement models

--- Volt/models.py
+++ Volt/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import Model
+from django.db.models import \
+    BigAutoField    as BigAuto, \
+    ForeignKey      as Foreign, \
+    BigIntegerField as BigInteger, \
+    FloatField      as Float, \
+    CharField       as Char
 
-# Create your models here.
+
+class Device(Model):
+    device_id = BigAuto(primary_key=True)
+
+
+class Measurement(Model):
+    class Meta:
+        unique_together = (('device_id', 'timestamp'),)
+
+    device_id = Foreign(Device, on_delete=models.CASCADE, primary_key=True, db_column='device_id')
+    timestamp = BigInteger()
+    mean = Float()
+    max = Float()
+    min = Float()
+
