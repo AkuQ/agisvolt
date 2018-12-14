98e86e86da14977c6e3ea8a5d2263695f9fc7b93
Akseli Kuistio <akuistio@gmail.com>
2018-12-14 15:04:24 +0200

    added mysql

--- agisvolt/settings.py
+++ agisvolt/settings.py
@@ -95,12 +95,7 @@ WSGI_APPLICATION = 'agisvolt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = env.databases
 
 
 # Password validation
