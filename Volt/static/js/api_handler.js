e6c46c9c637c0c46a65eb6d50c66a002106cfd1f
Akseli Kuistio <akuistio@gmail.com>
2019-02-09 12:24:32 +0200

    better redirect handling (needs more testing)

--- Volt/static/js/api_handler.js
+++ Volt/static/js/api_handler.js
@@ -25,19 +25,23 @@ function api_call(method, route, data={}) {
     else
         conf.body = JSON.stringify(data);
 
-    return fetch(route, conf);
+    let redirect_url;
+    return fetch(route, conf)
+    .then(res => {
+        if(res.redirected) redirect_url = res.url;
+        return res;
+    })
+    .finally(() => {
+        redirect_url && (window.location.href = redirect_url) && window.location.reload();
+    });
 }
-
 
 export function register(data) {
     return api_call('POST', '/register/', data)
-        .finally(() => window.location.reload());
 }
 export function login(data) {
     return api_call('POST', '/login/', data)
-        .finally(() => window.location.reload());
 }
 export function logout() {
     return api_call('POST', '/logout/')
-        .finally(() => window.location.reload());
 }
