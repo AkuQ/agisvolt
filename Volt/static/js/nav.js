e8575f350aa3b8df82678964bb497013470dd313
Akseli Kuistio <akuistio@gmail.com>
2019-02-26 17:59:07 +0200

    navbar, disable links based on perms, hide registration form for now

---
+++ Volt/static/js/nav.js
@@ -0,0 +1,27 @@
+import {logout} from "./api_handler";
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.li_style = {display: 'inline',  padding: 5};
+    }
+
+    render() {
+        return(
+            <ul style={{listStyleType: 'none', margin: 0, padding: 0}}>
+                <li style={ this.li_style }><a href={'/'}>Main</a></li>
+                <li style={ this.li_style }><a href={django.perms.monitor_device && '/devices'}>Devices</a></li>
+                <li style={ this.li_style } ><a href={django.perms.monitor_user && '/users'}>Users</a></li> {/* todo - can verify users*/}
+                {!django.user.is_authenticated && <li style={ this.li_style }><a  href={'/login'}>Login</a></li>}
+                { django.user.is_authenticated && <li style={ this.li_style }><a href={'/logout'}>Logout</a></li>}
+            </ul>
+        );
+    }
+}
+
+
+ReactDOM.render(<Nav/>, document.getElementById('nav'));
