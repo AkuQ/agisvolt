2025740f0844d2bd66b5112a6ea74e2128814388
Akseli Kuistio <akuistio@gmail.com>
2019-02-26 17:57:32 +0200

    changed/added permission constants

--- agisvolt/constants.py
+++ agisvolt/constants.py
@@ -1,6 +1,7 @@
 
 
 class PERM:
-    PROMOTE_USER = 'promote_supervisor'
+    MONITOR_USER = 'monitor_user'
+    PROMOTE_USER = 'promote_user'
     VERIFY_USER = 'verify_user'
     MONITOR_DEVICE = 'monitor_device'
