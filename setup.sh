9ca2820ad74fb0658afcd59a9eb93d592957181d
Akseli Kuistio <akuistio@gmail.com>
2019-03-03 17:40:51 +0200

    setup script, uwsgi via venv

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo "Creating python3.6 virtual environment..."
+python36 -m venv venv > /dev/null
+source venv/bin/activate > /dev/null
+
+echo "Installing required pip packages..."
+pip install -r requirements.txt > /dev/null
+
+echo "Creating systemd service file..."
+app_dir=$(pwd)
+echo '
+[Unit]
+Description=uWSGI instance to serve agisvolt
+
+[Service]
+ExecStartPre=-/usr/bin/bash -c "mkdir -p /run/agisvolt; chown nginx:nginx /run/agisvolt"
+ExecStart=/usr/bin/bash -c "cd '$app_dir'; source venv/bin/activate; uwsgi uwsgi/uwsgi.ini"
+
+[Install]
+WantedBy=multi-user.target
+' > uwsgi/agisvolt.service
+
+systemctl stop agisvolt &> /dev/null
+systemctl disable agisvolt &> /dev/null
+systemctl enable ${app_dir}/uwsgi/agisvolt.service
+systemctl start agisvolt
+
+
+echo "Compiling bundles..."
+./node_modules/.bin/webpack --config webpack.config.js > /dev/null
+
+
+deactivate
+echo "Done!"
