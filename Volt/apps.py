14e227442019d70bc6a3464a6816324e44393d81
Akseli Kuistio <akuistio@gmail.com>
2019-03-05 00:55:47 +0200

    removed uwsgi file watcher, caused managment issues

--- Volt/apps.py
+++ Volt/apps.py
@@ -1,17 +1,5 @@
 from django.apps import AppConfig
-from django.utils import autoreload
-
-import uwsgi
-from uwsgidecorators import timer
-
-from agisvolt.settings import DEBUG
 
 
 class VoltConfig(AppConfig):
     name = 'Volt'
-
-    if DEBUG:
-        @timer(3)
-        def change_code_gracefull_reload(sig):
-            if autoreload.code_changed():
-                uwsgi.reload()
